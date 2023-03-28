f981dd0e52e490f6e89bfc3fe7ee68282ad413bf
Afina R. Vinci <afinarv@gmail.com>
2023-03-28 09:35:50 +0700

    Initial commit.

--- BMICalculator/ContentView.swift
+++ BMICalculator/ContentView.swift
@@ -8,19 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var weight: String
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Spacer()
+            Text("The BMI Calculator")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            VStack(spacing: 10) {
+                Text("Your BMI Score").font(.title)
+                    .fontWeight(.bold)
+                Image("obesity")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:100)
+                Text("31.7").font(.title)
+                Text("Obsese").font(.title)
+            }.foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .background(Color(.systemBlue))
+                
+            
+            
+            
+            VStack {
+                HStack {
+                    Text("Weight:")
+                    TextField("Weight", text: .constant(""))
+                    Text("kg")
+                }
+                HStack {
+                    Text("Height:")
+                    TextField("Height", text: .constant(""))
+                    Text("cm")
+                }
+            }.font(.system(.title))
+                .padding(.leading, 40)
+                .padding(.trailing, 40)
+            
+            Button("Calculate") {
+    
+            }.buttonStyle(.borderedProminent)
+                .font(.largeTitle)
+                .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(weight: "40")
     }
 }
